ea62e60e1c9c5e186772f1ccf77b8c44ff843797
JR <95722025+Juniordee3@users.noreply.github.com>
2022-11-23 23:26:55 -0400

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+let cpuHand;
+
+// function getComputerChoice
+function getComputerChoice(min, max) {
+    // randomly generate number from 1 to 3 and assign to variable
+    const n = Math.floor(Math.random() * (max - min + 1) + min);
+    
+    if (n === 1)
+        cpuHand = "Rock";
+    else if (n === 2)
+        cpuHand = "Paper";
+    else if (n === 3)
+        cpuHand = "Scissors";
+}
+        // if number = 1
+            // return rock
+        // if number = 2
+            // return paper
+        // if number = 3
+            // return scissors
+getComputerChoice(1, 3);

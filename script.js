12c2e6bffaff3d9de618870a65e76a2dcd77d784
JR <95722025+Juniordee3@users.noreply.github.com>
2022-11-24 02:07:56 -0400

    Finish game

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let playerHand;
 let cpuHand;
 let playerScore = 0;
 let cpuScore = 0;
 
 // function getComputerChoice
 function getComputerChoice(min, max) {
+    
     // randomly generate number from 1 to 3 and assign to variable
     const n = Math.floor(Math.random() * (max - min + 1) + min);
     
@@ -14,59 +16,64 @@ function getComputerChoice(min, max) {
         cpuHand = "paper";
     else if (n === 3)
         cpuHand = "scissors";
-}
 
-// prompt for playerHand
-let playerHand = prompt("What will you play?");
-if (playerHand === null)
-    playerHand = prompt("Please enter rock, paper, or scissors");
-else
-    playerHand = playerHand.toLowerCase();
+    return cpuHand;
+}
 
 // function to play round
 function playRound(playerHand, cpuHand) {
 
+    // prompt for playerHand
+    playerHand = prompt("What will you play?");
+    if (playerHand === null)
+        playerHand = prompt("Please enter rock, paper, or scissors");
+    else
+        playerHand = playerHand.toLowerCase();
+
+    // get cpuHand
+    cpuHand = getComputerChoice(1, 3);
+
     // conditions for winning hand
     if (playerHand === "rock" && cpuHand === "rock")
-        return "Tie!";
+        console.log("Tie!");
     else if (playerHand === "rock" && cpuHand === "paper") {
-        cpuScore++;
-        return "Paper beats Rock! You lose!";
+        console.log("Paper beats Rock! You lose!");
+        return cpuScore++;
     } 
     else if (playerHand === "rock" && cpuHand === "scissors") {
-        playerScore++;
-        return "Rock beats Scissors! You win!";
+        console.log("Rock beats Scissors! You win!");
+        return playerScore++ ;
     }
     else if (playerHand === "paper" && cpuHand === "rock") {
-        playerScore++;
-        return "Paper beats Rock! You win!";
+        console.log("Paper beats Rock! You win!");
+        return playerScore++;
     }
     else if (playerHand === "paper" && cpuHand === "paper")
-        return "Tie!";
+        console.log("Tie!");
     else if (playerHand === "paper" && cpuHand === "scissors") {
-        cpuScore++;
-        return "Scissors beats Paper! You lose!";
+        console.log("Scissors beats Paper! You lose!");
+        return cpuScore++;
     }
     else if (playerHand === "scissors" && cpuHand === "rock") {
-        cpuScore++;
-        return "Rock beats Scissors! You lose!";
+        console.log("Rock beats Scissors! You lose!");
+        return cpuScore++;
     }
     else if (playerHand === "scissors" && cpuHand === "paper") {
-        playerScore++;
-        return "Scissors beats Paper! You win!";
+        console.log("Scissors beats Paper! You win!");
+        return playerScore++;
     }
     else if (playerHand === "scissors" && cpuHand === "scissors")
-        return "Tie!";
-
+        console.log("Tie!");
     else
-        return "Input not valid";
+        return playerScore, cpuScore;
 }
 
 // game function
 function game() {
     for (let i = 0; i < 5; i++) {
-
+        playRound(playerHand, cpuHand)
+        console.log("Player: " + playerScore + "\t" + "CPU: " + cpuScore);
     }
 }
-getComputerChoice(1, 3);
-console.log(playRound(playerHand, cpuHand));
+
+game();
